a824d17f6f2c708e50f960108e1874e4ce701700
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-03-28 19:07:20 +0200

    Aggiunto POST

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI;
 
 namespace SuperHeroAPI.Controllers
 {
@@ -7,30 +8,69 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        [HttpGet]
-
-        public async Task<ActionResult <List<SuperHero>>> Get()
-
+        private static List<SuperHero> heroes = new List<SuperHero>
         {
 
-            var heroes = new List<SuperHero>
-            {
+          new SuperHero {
 
-              new SuperHero 
-              { Id =1,
+               Id = 1,
                 Name = "Spider Man",
                 FirstName = "Peter",
                 LastName = "Parker",
                 Place = "New York City"
-              }
-         
-            
-            
-            };
-        
+          },
+
+             new SuperHero {
+
+               Id = 2,
+                Name = "Iron Man",
+                FirstName = "Tony",
+                LastName = "Stark",
+                Place = "Long Island"
+          }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+
+        {
+
             return Ok(heroes);
 
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Get(int id)
+
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+
+                return BadRequest("Hero not found");
+
+            return Ok(hero);
+
+        }
+
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
