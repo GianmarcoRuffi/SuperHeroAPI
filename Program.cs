744db20d451104e511463f5edcba6e17c814279a
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2023-03-31 10:20:48 +0200

    Aggiunta Repository e sostituita logica controller

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+//builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
 
 // add repositories
 
--- Services/SuperHeroService.cs
+++ Services/SuperHeroService.cs
@@ -1,41 +1,41 @@
-﻿using SuperHeroAPI.Data;
-using SuperHeroAPI;
-using Microsoft.EntityFrameworkCore;
+﻿//using SuperHeroAPI.Data;
+//using SuperHeroAPI;
+//using Microsoft.EntityFrameworkCore;
 
-public class SuperHeroService : ISuperHeroService
-{
-    private readonly ISuperHeroRepository _repository;
+//public class SuperHeroService : ISuperHeroService
+//{
+//    private readonly ISuperHeroRepository _repository;
 
-    public SuperHeroService(ISuperHeroRepository repository)
-    {
-        _repository = repository;
-    }
+//    public SuperHeroService(ISuperHeroRepository repository)
+//    {
+//        _repository = repository;
+//    }
 
-    public async Task<List<SuperHero>> GetHeroes()
-    {
-        return await _repository.GetHeroes();
-    }
+//    public async Task<List<SuperHero>> GetHeroes()
+//    {
+//        return await _repository.GetHeroes();
+//    }
 
-    public async Task<SuperHero> GetHeroById(int id)
-    {
-        return await _repository.GetHeroById(id);
-    }
+//    public async Task<SuperHero> GetHeroById(int id)
+//    {
+//        return await _repository.GetHeroById(id);
+//    }
 
-    public async Task<List<SuperHero>> AddHero(SuperHero hero)
-    {
-        await _repository.AddHero(hero);
-        return await _repository.GetHeroes();
-    }
+//    public async Task<List<SuperHero>> AddHero(SuperHero hero)
+//    {
+//        await _repository.AddHero(hero);
+//        return await _repository.GetHeroes();
+//    }
 
-    public async Task<List<SuperHero>> UpdateHero(SuperHero request)
-    {
-        await _repository.UpdateHero(request);
-        return await _repository.GetHeroes();
-    }
+//    public async Task<List<SuperHero>> UpdateHero(SuperHero request)
+//    {
+//        await _repository.UpdateHero(request);
+//        return await _repository.GetHeroes();
+//    }
 
-    public async Task<List<SuperHero>> DeleteHero(int id)
-    {
-        await _repository.DeleteHero(id);
-        return await _repository.GetHeroes();
-    }
-}
+//    public async Task<List<SuperHero>> DeleteHero(int id)
+//    {
+//        await _repository.DeleteHero(id);
+//        return await _repository.GetHeroes();
+//    }
+//}
